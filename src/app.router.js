6ca948d6b98e34a3f305932cd9ddb2424b07a06d
TaimaTN <taima598awwad@gmail.com>
2024-05-09 00:30:09 +0300

    set the enveroment

--- src/app.router.js
+++ src/app.router.js
@@ -1,17 +1,20 @@
+import cors from "cors";
 import connectDB from '../DB/conniection.js';
 import catagoryRouter from './modules/catagory/router.js';
 import productRouter from './modules/product/router.js';
-const initApp=(app,express)=>{
-    connectDB();
-   app.get('/',(req,res)=>{
-    return res.json('Welcome in ecommerCE..')
-   }) ;
-   app.use('/catagories',catagoryRouter);
-   app.use('/products',productRouter);
 
-   app.use('/*',(req,res)=>{
-    return res.status(404).json({message:"page not found"});
- })
+const initApp = (app, express) => {
+    connectDB();
+    app.use(cors());
+    app.get('/', (req, res) => {
+        return res.json('Welcome in ecommerCE..')
+    });
+    app.use('/catagories', catagoryRouter);
+    app.use('/products', productRouter);
+
+    app.use('/*', (req, res) => {
+        return res.status(404).json({ message: "page not found" });
+    })
 }
 
 export default initApp;
